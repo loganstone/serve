402fefc9e95afbe2921d0cd9088b1c40bd230895
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-03-06 00:37:47 +0900

    서버 실행 후 접속 테스트 추가

--- serve_test.go
+++ serve_test.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"gotest.tools/assert"
 )
+
+func newTestServer() *httptest.Server {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", portToListen))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := wrapHandlerWithLogging(http.FileServer(http.Dir(dirToServe)))
+	ts := httptest.NewUnstartedServer(handler)
+	ts.Listener.Close()
+	ts.Listener = ln
+	return ts
+}
 
 func TestAbsPath(t *testing.T) {
 	actual, err := absPath(".")
@@ -19,4 +39,39 @@ func TestAbsPath(t *testing.T) {
 	}
 
 	assert.Equal(t, actual, expected)
+}
+
+func TestRunServe(t *testing.T) {
+	dirToServe = "."
+	portToListen = 9000
+
+	ts := newTestServer()
+
+	ts.Start()
+	client := ts.Client()
+	defer ts.Close()
+
+	resp, err := client.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("received non-200 response: %d\n", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(body)
+	files, err := ioutil.ReadDir(dirToServe)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, f := range files {
+		expected := f.Name()
+		assert.Assert(t, strings.Contains(actual, expected))
+	}
 }
