0177b202646c42a45365a014c91997a17a01d666
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-08-03 21:48:36 +0900

    문서화를 위해 conf.go 에 주석을 추가

--- conf/conf.go
+++ conf/conf.go
@@ -3,13 +3,13 @@ package conf
 import "flag"
 
 const (
-	// DefaultDir ...
+	// DefaultDir -d 옵션 값이 입력되지 않았을때 사용된다.
 	DefaultDir = "."
-	// DefaultPort ...
+	// DefaultPort -p 옵션 값이 입력되지 않았을대 사용된다.
 	DefaultPort = 9000
 )
 
-// Options .
+// Options CLI 입력 옵션을 타입화.
 type Options struct {
 	DirToServe   string
 	PortToListen int
@@ -23,7 +23,7 @@ func init() {
 	flag.Parse()
 }
 
-// Opts .
+// Opts 'init' 에서 처리 된 -d, -p 값을 담은 type 을 반환하다.
 func Opts() Options {
 	return opts
 }
