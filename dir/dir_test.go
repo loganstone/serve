c463fa3f4cc377174094eb26336d58bf71c3179c
Logan <hs.lee.logan@gmail.com>
2020-05-02 11:00:00 +0900

    NewWatcher 테스트 추가

--- dir/dir_test.go
+++ dir/dir_test.go
@@ -1,6 +1,7 @@
 package dir
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -18,4 +19,12 @@ func TestAbs(t *testing.T) {
 	testPath := "/abs/path"
 	_, err = Abs(testPath)
 	assert.NoError(t, err)
+}
+
+func TestNewWatcher(t *testing.T) {
+	_, err := NewWatcher(".")
+	assert.NoError(t, err)
+
+	_, err = NewWatcher("bad_dir")
+	assert.Error(t, errors.New("lstat bad_dir: no such file or directory"), err)
 }
