b1ee081467f6d6b3e1905e9bc82cc54905e1069a
Logan <hs.lee.logan@gmail.com>
2020-05-06 16:50:55 +0900

    서버 테스트 개선 - 고루틴안에 Fatal 함수 제거

--- server/server_test.go
+++ server/server_test.go
@@ -3,8 +3,10 @@ package server
 import (
 	"errors"
 	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,66 +14,70 @@ import (
 	"github.com/loganstone/serve/conf"
 )
 
-func TestRunServeAndReqeust(t *testing.T) {
+var testLn net.Listener
+var testSrv *http.Server
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
 	ln, err := Listener(conf.DefaultPort)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	srv := newServer(conf.DefaultDir, ln.Addr().(*net.TCPAddr).Port, false)
-	errc := make(chan error, 1)
+	testLn = ln
+	testSrv = newServer(conf.DefaultDir, testLn.Addr().(*net.TCPAddr).Port, false)
+	go testSrv.Serve(ln)
+}
 
-	go func() {
-		retry := 0
-		for {
-			resp, err := http.Get("http://" + ln.Addr().String())
-			if err != nil {
-				if retry > 4 {
-					t.Fatal(err)
-				}
-				retry++
-				continue
-			}
+func teardown() {
+	testLn.Close()
+	testSrv.Close()
+}
 
-			if resp.StatusCode != 200 {
-				t.Fatalf("received non-200 response: %d\n", resp.StatusCode)
-			}
-
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
+func TestRequest(t *testing.T) {
+	retry := 0
+	for {
+		resp, err := http.Get("http://" + testLn.Addr().String())
+		if err != nil {
+			if retry > 4 {
 				t.Fatal(err)
 			}
-
-			actual := string(body)
-			files, err := ioutil.ReadDir(conf.DefaultDir)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			for _, f := range files {
-				expected := f.Name()
-				assert.Contains(t, actual, expected)
-			}
-			defer resp.Body.Close()
-			defer ln.Close()
-			defer srv.Close()
-			break
+			retry++
+			continue
 		}
-	}()
 
-	go func() {
-		errc <- srv.Serve(ln)
-	}()
-	<-errc
+		if resp.StatusCode != 200 {
+			t.Fatalf("received non-200 response: %d\n", resp.StatusCode)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		actual := string(body)
+		files, err := ioutil.ReadDir(conf.DefaultDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for _, f := range files {
+			expected := f.Name()
+			assert.Contains(t, actual, expected)
+		}
+		defer resp.Body.Close()
+		break
+	}
 }
 
 func TestIsErrorAddressAlreadyInUse(t *testing.T) {
-	firstLn, err := Listener(conf.DefaultPort)
-	defer firstLn.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	secondLn, err := Listener(conf.DefaultPort)
-	defer secondLn.Close()
+	ln, err := Listener(conf.DefaultPort)
+	defer ln.Close()
 	assert.True(t, IsErrorAddressAlreadyInUse(err))
 
 	err = errors.New("some error")
