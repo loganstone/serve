91a4979c57899805e0262f5638f6fb91915ef468
Logan <hs.lee.logan@gmail.com>
2020-05-21 18:33:01 +0900

    모호한 주석을 명확하게 수정

--- main.go
+++ main.go
@@ -25,9 +25,10 @@ func main() {
 	defer watcher.Close()
 	go watcher.NowMyWatchBegins()
 
-	// Listener port 사용 검사는 생성 시 에러로 확인하는 것이
-	// 코드가 명료하고 간단하다.
-	// prot 확인과 생성 사이의 시간에 다른 process 에서 port 를 점유한다면,
+	// Listener port 가 이미 사용되는지 확인은
+	// 생성 시 에러로 확인하는 것이 코드가 명료하고 간단하다.
+	// prot 확인과 생성 코드가 분리되어있다면,
+	// 실행 사이의 시간에 다른 process 에서 port 를 점유할 수 있어,
 	// 에러가 발생하기는 마찬가지이고, 코드가 더 복잡해진다.
 	ln, err := server.Listener(opts.PortToListen)
 	if server.IsErrorAddressAlreadyInUse(err) {
